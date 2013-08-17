a067b916e47a887d5467fee702e0b19bb8e5c4a1
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-08-17 20:55:20 +0200

    Added correct listing for static files.

--- official-turbo-site.lua
+++ official-turbo-site.lua
@@ -4,10 +4,13 @@
 local turbo = require "turbo"
 
 local app = turbo.web.Application:new({
-	{"/(.*)$", turbo.web.StaticFileHandler, "./"}
+	{"^/$", turbo.web.StaticFileHandler, "./index.html"},
+	{"^/doc/$", turbo.web.StaticFileHandler, "./doc/index.html"},
+	{"^/doc/(.*)$", turbo.web.StaticFileHandler, "./doc/"},
+	{"^/lib/(.*)$", turbo.web.StaticFileHandler, "./lib/"},
 })
 
 local srv = turbo.httpserver.HTTPServer(app)
 srv:bind(8888)
-srv:start(4)
+srv:start(1)
 turbo.ioloop.instance():start()
