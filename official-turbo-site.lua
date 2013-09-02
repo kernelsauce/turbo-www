5db5157a35f0799d1e53d3f340b5cc26e9da29bd
John Abrahamsen <jhnabrhmsn@gmail.com>
2013-09-02 20:24:00 +0200

    Some minor changes.

--- official-turbo-site.lua
+++ official-turbo-site.lua
@@ -3,10 +3,18 @@
 
 local turbo = require "turbo"
 
+local Templater = class("Templater", turbo.web.RequestHandler)
+local tmpl = turbo.web.Mustache.TemplateHelper("./")
 
+function Templater:get(name)
+	local index = tmpl:load("index.mustache")
+	local menu = tmpl:render("menu.mustache")
+	local page = tmpl:render(index, {menu = menu})
+	self:write(page)
+end
 
-local app = turbo.web.Application:new({
-	{"^/$", turbo.web.StaticFileHandler, "./index.html"},
+local app = turbo.web.Application({
+	{"^/$", Templater},
 	{"^/doc/$", turbo.web.StaticFileHandler, "./doc/index.html"},
 	{"^/doc/(.*)$", turbo.web.StaticFileHandler, "./doc/"},
 	{"^/lib/(.*)$", turbo.web.StaticFileHandler, "./lib/"},
@@ -15,5 +23,5 @@ local app = turbo.web.Application:new({
 
 local srv = turbo.httpserver.HTTPServer(app)
 srv:bind(8888)
-srv:start(1)
+srv:start(4)
 turbo.ioloop.instance():start()
