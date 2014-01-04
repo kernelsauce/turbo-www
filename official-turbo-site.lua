062b6c08950d5beefb3a395c971e80bb7d19595d
John Abrahamsen <jhnabrhmsn@gmail.com>
2014-01-04 14:02:35 +0100

    Tabs to spaces...

--- official-turbo-site.lua
+++ official-turbo-site.lua
@@ -7,37 +7,37 @@ local SFH = turbo.web.StaticFileHandler
 
 local Index = class("Index", turbo.web.RequestHandler)
 function Index:get()
-	local index = tmpl:load("index.mustache")
-	local page = tmpl:render(index, {
-		menu = tmpl:render("menu.mustache"), 
-		foot = tmpl:render("foot.mustache"), 
-		head = tmpl:render("header.mustache")
-	})
-	self:write(page)
+    local index = tmpl:load("index.mustache")
+    local page = tmpl:render(index, {
+        menu = tmpl:render("menu.mustache"), 
+        foot = tmpl:render("foot.mustache"), 
+        head = tmpl:render("header.mustache")
+    })
+    self:write(page)
 end
 
 local TemplateRenderer = class("TemplateRenderer", turbo.web.RequestHandler)
 function TemplateRenderer:get()
-	local page = tmpl:render(self.options)
-	self:write(page)
+    local page = tmpl:render(self.options)
+    self:write(page)
 end
 
 local app = turbo.web.Application({
-	{"^/$", 			Index},
-	{"^/performance$", 	TemplateRenderer, 	"performance.mustache"},
-	{"^/lua$", 			TemplateRenderer, 	"lua.mustache"},
-	{"^/community$", 	TemplateRenderer, 	"community.mustache"},
-	{"^/download$", 	TemplateRenderer, 	"download.mustache"},
-	{"^/code$", 		TemplateRenderer, 	"code.mustache"},
-	{"^/modules$", 		TemplateRenderer, 	"modules.mustache"},
-	{"^/sponsor$", 		TemplateRenderer, 	"sponsor.mustache"},
-	-- End of templates.
-	{"^/doc/$", 		SFH, 				"./doc/index.html"},
-	{"^/doc/(.*)$", 	SFH, 				"./doc/"},
-	{"^/lib/(.*)$", 	SFH, 				"./lib/"},
-	{"^/assets/(.*)$", 	SFH, 				"./assets/"},
-	-- Redirect to Get started guide.
-	{"^/gettingstarted$", turbo.web.RedirectHandler, "/doc/get_started.html"},
+    {"^/$",             Index},
+    {"^/performance$",  TemplateRenderer,   "performance.mustache"},
+    {"^/lua$",          TemplateRenderer,   "lua.mustache"},
+    {"^/community$",    TemplateRenderer,   "community.mustache"},
+    {"^/download$",     TemplateRenderer,   "download.mustache"},
+    {"^/code$",         TemplateRenderer,   "code.mustache"},
+    {"^/modules$",      TemplateRenderer,   "modules.mustache"},
+    {"^/sponsor$",      TemplateRenderer,   "sponsor.mustache"},
+    -- End of templates.
+    {"^/doc/$",         SFH,                "./doc/index.html"},
+    {"^/doc/(.*)$",     SFH,                "./doc/"},
+    {"^/lib/(.*)$",     SFH,                "./lib/"},
+    {"^/assets/(.*)$",  SFH,                "./assets/"},
+    -- Redirect to Get started guide.
+    {"^/gettingstarted$", turbo.web.RedirectHandler, "/doc/get_started.html"},
 })
 
 local srv = turbo.httpserver.HTTPServer(app)
