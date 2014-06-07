0e4b749c3eb805bf237afedebe3b7cae37b12b7d
John Abrahamsen <jhnabrhmsn@gmail.com>
2014-06-07 10:32:55 +0200

    Removed logging to Couch.

--- official-turbo-site.lua
+++ official-turbo-site.lua
@@ -6,36 +6,6 @@ local turbo = require "turbo"
 local tmpl = turbo.web.Mustache.TemplateHelper("./")
 local SFH = turbo.web.StaticFileHandler
 
-local gioloop = turbo.ioloop.instance()
-local function _set_content_type(header)
-    header:add("Content-Type", "application/json")
-end
-local function _log_line_cb(log)
-    if log[1]:find("[async.lua]", 0, true) then
-        -- Do not log HTTPClient.
-        io.stdout:write(log[1].."\n")
-        return
-    end
-    local res = coroutine.yield(turbo.async.HTTPClient():fetch(
-        "http://127.0.0.1:5984/turbo-www", {
-            method="POST",
-            body=turbo.escape.json_encode({
-                timestamp=turbo.util.gettimeofday(),
-                line=log[1],
-                logtype=log[2]
-            }),
-            on_headers=_set_content_type
-        }))
-    if res.error then
-        gioloop:add_callback(_log_line_cb, log)
-    end
-end
-
-turbo.log.success = function(str) gioloop:add_callback(_log_line_cb, {str, "success"}) end
-turbo.log.notice = function(str) gioloop:add_callback(_log_line_cb, {str, "notice"}) end
-turbo.log.error = function(str) gioloop:add_callback(_log_line_cb, {str, "error"}) end
-turbo.log.warning = function(str) gioloop:add_callback(_log_line_cb, {str, "warning"}) end
-
 local TemplateRenderer = class("TemplateRenderer", turbo.web.RequestHandler)
 function TemplateRenderer:get()
     local page = tmpl:render(self.options, {
